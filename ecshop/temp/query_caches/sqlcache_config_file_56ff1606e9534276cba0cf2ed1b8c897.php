18b4918d6d3c0e81884f137ac478b4cb0f417f7d
unknown <Administrator@.(none)>
2011-08-21 16:30:01 +0800

    add a theme xk

--- ecshop/temp/query_caches/sqlcache_config_file_56ff1606e9534276cba0cf2ed1b8c897.php
+++ ecshop/temp/query_caches/sqlcache_config_file_56ff1606e9534276cba0cf2ed1b8c897.php
@@ -1,5 +1,5 @@
 <?php
-$this->mysql_config_cache_file_time = 1313897582;
+$this->mysql_config_cache_file_time = 1313938493;
 $this->timeline = 0;
 $this->timezone = 28800;
 $this->platform = 'OTHER';
--- ecshop/temp/static_caches/cat_pid_releate.php
+++ ecshop/temp/static_caches/cat_pid_releate.php
@@ -2,6 +2,19 @@
 $data = array (
   0 => 
   array (
+    'cat_id' => '6',
+    'cat_name' => '虚拟主机',
+    'measure_unit' => '',
+    'parent_id' => '0',
+    'is_show' => '1',
+    'show_in_nav' => '1',
+    'grade' => '0',
+    'sort_order' => '50',
+    'has_children' => '4',
+    'goods_num' => 0,
+  ),
+  1 => 
+  array (
     'cat_id' => '1',
     'cat_name' => '建站套餐',
     'measure_unit' => '',
@@ -13,7 +26,7 @@ $data = array (
     'has_children' => '4',
     'goods_num' => 0,
   ),
-  1 => 
+  2 => 
   array (
     'cat_id' => '12',
     'cat_name' => '域名注册',
@@ -26,46 +39,7 @@ $data = array (
     'has_children' => '3',
     'goods_num' => 0,
   ),
-  2 => 
-  array (
-    'cat_id' => '6',
-    'cat_name' => '虚拟主机',
-    'measure_unit' => '',
-    'parent_id' => '0',
-    'is_show' => '1',
-    'show_in_nav' => '1',
-    'grade' => '0',
-    'sort_order' => '50',
-    'has_children' => '4',
-    'goods_num' => 0,
-  ),
   3 => 
-  array (
-    'cat_id' => '5',
-    'cat_name' => '标准套餐',
-    'measure_unit' => '',
-    'parent_id' => '1',
-    'is_show' => '1',
-    'show_in_nav' => '1',
-    'grade' => '5',
-    'sort_order' => '50',
-    'has_children' => '0',
-    'goods_num' => '1',
-  ),
-  4 => 
-  array (
-    'cat_id' => '2',
-    'cat_name' => '商务套餐',
-    'measure_unit' => '',
-    'parent_id' => '1',
-    'is_show' => '1',
-    'show_in_nav' => '0',
-    'grade' => '0',
-    'sort_order' => '50',
-    'has_children' => '0',
-    'goods_num' => '1',
-  ),
-  5 => 
   array (
     'cat_id' => '3',
     'cat_name' => '低价打折',
@@ -78,7 +52,7 @@ $data = array (
     'has_children' => '0',
     'goods_num' => '1',
   ),
-  6 => 
+  4 => 
   array (
     'cat_id' => '4',
     'cat_name' => '精品套餐',
@@ -91,20 +65,33 @@ $data = array (
     'has_children' => '0',
     'goods_num' => 0,
   ),
-  7 => 
+  5 => 
   array (
-    'cat_id' => '8',
-    'cat_name' => '推荐主机',
+    'cat_id' => '5',
+    'cat_name' => '标准套餐',
     'measure_unit' => '',
-    'parent_id' => '6',
+    'parent_id' => '1',
+    'is_show' => '1',
+    'show_in_nav' => '1',
+    'grade' => '5',
+    'sort_order' => '50',
+    'has_children' => '0',
+    'goods_num' => '1',
+  ),
+  6 => 
+  array (
+    'cat_id' => '2',
+    'cat_name' => '商务套餐',
+    'measure_unit' => '',
+    'parent_id' => '1',
     'is_show' => '1',
     'show_in_nav' => '0',
     'grade' => '0',
     'sort_order' => '50',
     'has_children' => '0',
-    'goods_num' => 0,
+    'goods_num' => '1',
   ),
-  8 => 
+  7 => 
   array (
     'cat_id' => '9',
     'cat_name' => '商务主机',
@@ -117,7 +104,7 @@ $data = array (
     'has_children' => '0',
     'goods_num' => 0,
   ),
-  9 => 
+  8 => 
   array (
     'cat_id' => '11',
     'cat_name' => '不可能再低了',
@@ -130,7 +117,7 @@ $data = array (
     'has_children' => '0',
     'goods_num' => 0,
   ),
-  10 => 
+  9 => 
   array (
     'cat_id' => '7',
     'cat_name' => '标准主机',
@@ -143,20 +130,20 @@ $data = array (
     'has_children' => '0',
     'goods_num' => 0,
   ),
-  11 => 
+  10 => 
   array (
-    'cat_id' => '15',
-    'cat_name' => '联通手机充值卡',
+    'cat_id' => '8',
+    'cat_name' => '推荐主机',
     'measure_unit' => '',
-    'parent_id' => '12',
-    'is_show' => '0',
+    'parent_id' => '6',
+    'is_show' => '1',
     'show_in_nav' => '0',
     'grade' => '0',
     'sort_order' => '50',
     'has_children' => '0',
     'goods_num' => 0,
   ),
-  12 => 
+  11 => 
   array (
     'cat_id' => '13',
     'cat_name' => '小灵通/固话充值卡',
@@ -169,13 +156,26 @@ $data = array (
     'has_children' => '0',
     'goods_num' => 0,
   ),
-  13 => 
+  12 => 
   array (
     'cat_id' => '14',
     'cat_name' => '域名注册',
     'measure_unit' => '',
     'parent_id' => '12',
     'is_show' => '1',
+    'show_in_nav' => '0',
+    'grade' => '0',
+    'sort_order' => '50',
+    'has_children' => '0',
+    'goods_num' => 0,
+  ),
+  13 => 
+  array (
+    'cat_id' => '15',
+    'cat_name' => '联通手机充值卡',
+    'measure_unit' => '',
+    'parent_id' => '12',
+    'is_show' => '0',
     'show_in_nav' => '0',
     'grade' => '0',
     'sort_order' => '50',
