a24adb6bf7482b76de93846de35c212757f5cca7
cdjflxk <736233152@qq.com>
2011-09-01 18:06:23 +0800

    try to module this project

---
+++ xkshop/lib/Action/Home/IndexAction.class.php
@@ -0,0 +1,56 @@
+<?php
+
+class IndexAction extends Action {
+
+    public function index() {
+        import("@.Home.IndexModelAction");
+        $indexModelAction = new IndexModelAction();
+        $map = $indexModelAction->queryAllToMap();
+        $this->assign('rootPath', "./Tpl/default/Home/");
+        $this->assign('title', $map['title']);
+        $this->assign('keywords', $map['keywords']);
+        $this->assign('description', $map['description']);
+        $this->assign('phone', $map['phone']);
+        $this->display();
+    }
+
+    public function dns() {
+        $this->display();
+    }
+
+    public function virtual() {
+        $this->display();
+    }
+
+    public function email() {
+        $this->display();
+    }
+
+    public function vps() {
+        $this->display();
+    }
+
+    public function shop() {
+        $this->display();
+    }
+
+    public function site() {
+        $this->display();
+    }
+
+    public function helpcenter() {
+        $this->display();
+    }
+
+    public function business() {
+        $this->display();
+    }
+    
+    public function login() {
+        
+        $this->display();
+    }
+
+}
+
+?>
