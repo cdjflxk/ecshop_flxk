ff38fb41344654ae2f2a28b9b4ce48078cf20642
cdjflxk <736233152@qq.com>
2011-09-07 18:19:21 +0800

    virtual product's add

--- xkshop/Lib/Action/SuperAdmin/IndexAction.class.php
+++ xkshop/Lib/Action/SuperAdmin/IndexAction.class.php
@@ -2,12 +2,12 @@
 class IndexAction extends Action {
 
     public function index() {
- //       $this->assign('__SuperAdmin__', __SuperAdmin__);
+       $this->assign('__SuperAdmin__', __SuperAdmin__);
         $this->display();
     }
     
     public function top() {
-       //$this->assign('__CommonAdmin__', __CommonAdmin__);
+       $this->assign('__CommonAdmin__', __CommonAdmin__);
         $this->display();
     }
     
@@ -16,9 +16,29 @@ class IndexAction extends Action {
     }
     
     public function menu() {
+    	$this->assign('__SuperAdmin__', __SuperAdmin__);
         $this->display();
     }
     public function bottom() {
+        $this->display();
+    }
+    
+    public function pd_lst() {
+    	$type = $_GET["type"];
+    	if(type == "index"){
+    		$this->assign('type', 'index');
+          	$this->display();  		
+    	}else if(type == "virtual"){
+    		
+    	}
+
+    }
+    
+    public function pd_tpe() {
+        $this->display();
+    }
+    
+    public function pd_r() {
         $this->display();
     }
 }
